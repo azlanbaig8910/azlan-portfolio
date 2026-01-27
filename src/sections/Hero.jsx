dbe750d7ae5d6334d81b9e135c8edc68182ea335
Azlan Baig <azlanbaig8910@gmail.com>
2026-01-28 00:25:37 +0500

    updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -73,7 +73,7 @@ export const Hero = () => {
                 </span>
               </h1>
               <p className="text-lg text-muted-foreground max-w-lg animate-fade-in animation-delay-200">
-                Hi, I’m Azlan, a Frontend Developer with 1+ year of experience building responsive, user-focused interfaces using React, Next.js, Typescript and modern CSS frameworks.
+                Hi, I’m Azlan, a Frontend Developer with 1+ year of experience building responsive, user-focused interfaces using React, Next.js, TypeScript and modern CSS frameworks.
               </p>
             </div>
 
